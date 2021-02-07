69483e364e18c1235883ba71e37ea64519cd9878
nbolinaga <nbolinaga17@gmail.com>
2021-02-07 00:52:56 -0400

    agregado login modal

--- automotrizBGMQ/src/app/login/login.component.ts
+++ automotrizBGMQ/src/app/login/login.component.ts
@@ -1,15 +1,55 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition
+} from '@angular/animations';
+
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
+  animations: [
+    trigger('leftAnimation', [
+      transition(':enter', [
+        style({ "left": "-100%", "opacity": "0" }, ),
+        animate('.5s ease-out', style({ "left": "50%", "opacity": "1"}))
+      ]),
+      transition(':leave', [
+        style({ "left": "50%", "opacity": "1" }),
+        animate('.5s ease-in', style({ "left": "-100%", "opacity": "0"}))
+      ])
+    ]),
+    trigger('rightAnimation', [
+      transition(':enter', [
+        style({ "right": "-100%", "opacity": "0" }, ),
+        animate('.5s ease-out', style({ "right": "50%", "opacity": "1"}))
+      ]),
+      transition(':leave', [
+        style({ "right": "50%", "opacity": "1" }),
+        animate('.5s ease-in', style({ "right": "-100%", "opacity": "0"}))
+      ])
+    ]),
+  ]
 })
 export class LoginComponent implements OnInit {
+
+  registrar: Boolean = false;
+  
+ 
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  ingresarScreen(){
+    this.registrar = false;
+  }
+  registrarScreen(){
+    this.registrar = true;
+  }
 }
