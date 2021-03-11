3ea17b017af77e97fb81324ee8b6695699eadab0
nbolinaga <nbolinaga17@gmail.com>
2021-03-11 09:03:18 -0400

    fixed

--- automotrizBGMQ/src/app/app.module.ts
+++ automotrizBGMQ/src/app/app.module.ts
@@ -38,6 +38,7 @@ const routes: Routes = [
   {path: 'perfil', component: PerfilComponent},
   {path: 'autos', component: AutosComponent},
   {path: 'admin', component: AdminComponent}
+]
 
 @NgModule({
   declarations: [
