ed5c48e68be9c0fc080a3cf0a570da2437d800c4
JendersonQuinter <j.quintero@correo.unimet.edu.ve>
2021-03-10 23:56:07 -0400

    Creacion de listas estaticas en Citas

--- automotrizBGMQ/src/app/components/citas/citas.component.ts
+++ automotrizBGMQ/src/app/components/citas/citas.component.ts
@@ -8,6 +8,155 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CitasComponent implements OnInit {
 
+  // Variables de Muestra:
+  citasDia: Array<any> = [
+    {
+      nombre: 'Pedro Perez',
+      motivo: 'Cambio de Aceite'
+    },
+    {
+      nombre: 'Maria Parra',
+      motivo: 'Servicio completo'
+    },
+    {
+      nombre: 'Carlos Chavez',
+      motivo: 'Cambio de Neumaticos'
+    },
+    {
+      nombre: 'Mario Benitez',
+      motivo: 'Revision'
+    },
+    {
+      nombre: 'Luis Alcala',
+      motivo: 'Cambio de pastillas de freno'
+    },
+    {
+      nombre: 'Marcos Mora',
+      motivo: 'Servicio completo'
+    },
+    {
+      nombre: 'Miguel Perensejo',
+      motivo: 'Reparación'
+    },
+    {
+      nombre: 'Alonso Hernandez',
+      motivo: 'Revision'
+    },
+    {
+      nombre: 'Camilo Turizo',
+      motivo: 'Cambio de aceite'
+    },
+    {
+      nombre: 'Matilda Hilda',
+      motivo: 'Servicio completo'
+    }
+  ];
+
+  citasSolicitudes: Array<any> = [
+    {
+      nombre: 'Pedro Perez',
+      motivo: 'Cambio de Aceite',
+      fecha: 'DD/MM/AA'
+    },
+    {
+      nombre: 'Maria Parra',
+      motivo: 'Servicio completo',
+      fecha: 'DD/MM/AA'
+    },
+    {
+      nombre: 'Carlos Chavez',
+      motivo: 'Cambio de Neumaticos',
+      fecha: 'DD/MM/AA'
+    },
+    {
+      nombre: 'Mario Benitez',
+      motivo: 'Revision',
+      fecha: 'DD/MM/AA'
+    },
+    {
+      nombre: 'Luis Alcala',
+      motivo: 'Cambio de pastillas de freno',
+      fecha: 'DD/MM/AA'
+    },
+    {
+      nombre: 'Marcos Mora',
+      motivo: 'Servicio completo',
+      fecha: 'DD/MM/AA'
+    },
+    {
+      nombre: 'Miguel Perensejo',
+      motivo: 'Reparación',
+      fecha: 'DD/MM/AA'
+    },
+    {
+      nombre: 'Alonso Hernandez',
+      motivo: 'Revision',
+      fecha: 'DD/MM/AA'
+    },
+    {
+      nombre: 'Camilo Turizo',
+      motivo: 'Cambio de aceite',
+      fecha: 'DD/MM/AA'
+    },
+    {
+      nombre: 'Matilda Hilda',
+      motivo: 'Servicio completo',
+      fecha: 'DD/MM/AA'
+    }
+  ];
+
+  citasEstatus: Array<any> = [
+    {
+      nombre: 'Pedro Perez',
+      motivo: 'Cambio de Aceite',
+      estado: 'Esperando Confirmación'
+    },
+    {
+      nombre: 'Maria Parra',
+      motivo: 'Servicio completo',
+      estado: 'Confirmada'
+    },
+    {
+      nombre: 'Carlos Chavez',
+      motivo: 'Cambio de Neumaticos',
+      estado: 'Rechazada'
+    },
+    {
+      nombre: 'Mario Benitez',
+      motivo: 'Revision',
+      estado: 'Esperando Confirmación'
+    },
+    {
+      nombre: 'Luis Alcala',
+      motivo: 'Cambio de pastillas de freno',
+      estado: 'Esperando Confirmación'
+    },
+    {
+      nombre: 'Marcos Mora',
+      motivo: 'Servicio completo',
+      estado: 'Rechazada'
+    },
+    {
+      nombre: 'Miguel Perensejo',
+      motivo: 'Reparación',
+      estado: 'Rechazada'
+    },
+    {
+      nombre: 'Alonso Hernandez',
+      motivo: 'Revision',
+      estado: 'Confirmada'
+    },
+    {
+      nombre: 'Camilo Turizo',
+      motivo: 'Cambio de aceite',
+      estado: 'Confirmada'
+    },
+    {
+      nombre: 'Matilda Hilda',
+      motivo: 'Servicio completo',
+      estado: 'Confirmada'
+    }
+  ];
 
   constructor() { }
 
