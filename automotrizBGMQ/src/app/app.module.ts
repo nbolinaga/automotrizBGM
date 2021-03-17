b2591b5ea80820f7279144cc1499e4dd53612e3e
JendersonQuinter <j.quintero@correo.unimet.edu.ve>
2021-03-16 20:14:11 -0400

    Merge de Micale y main en Jenderson

--- automotrizBGMQ/src/app/app.module.ts
+++ automotrizBGMQ/src/app/app.module.ts
@@ -1,21 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
-import {AngularFireModule} from '@angular/fire';
-import {AngularFirestoreModule} from '@angular/fire/firestore';
-import {AngularFireStorageModule} from '@angular/fire/storage';
-import {AngularFireAuthModule} from '@angular/fire/auth';
-import {environment} from 'src/environments/environment';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { environment } from 'src/environments/environment';
 
 import { InicioComponent } from './components/inicio/inicio.component';
-import { LandingPageComponent } from './components/landing-page/landing-page.component';
-import { VisitanosComponent } from './components/visitanos/visitanos.component';
-import { ContactanosComponent } from './components/contactanos/contactanos.component';
-import { QuienesSomosComponent } from './components/quienes-somos/quienes-somos.component';
+
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { LoginComponent } from './components/login/login.component';
@@ -27,19 +24,14 @@ import { CalendarioComponent } from './components/calendario/calendario.componen
 
 import {AuthService} from './services/auth.service';
 import { ReactiveFormsModule } from '@angular/forms';
+import { ContactanosComponent } from './components/contactanos/contactanos.component';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { QuienesSomosComponent } from './components/quienes-somos/quienes-somos.component';
+import { VisitanosComponent } from './components/visitanos/visitanos.component';
 
 const routes: Routes = [
-  {path: '', component: LandingPageComponent},
-  {path: 'inicio', component: InicioComponent},
-  {path: 'quienesSomos', component: QuienesSomosComponent},
-  {path: 'visitanos', component: VisitanosComponent},
-  {path: 'contactanos', component: ContactanosComponent},
-  {path: 'login', component: LoginComponent},
-  {path: 'citas', component: CitasComponent},
-  {path: 'perfil', component: PerfilComponent},
-  {path: 'autos', component: AutosComponent},
-  {path: 'admin', component: AdminComponent},
-  {path: 'calendario', component: CalendarioComponent}
+  {path: '', component: InicioComponent},
+  {path: 'autos', component: AutosComponent}
 ];
 
 @NgModule({
@@ -49,6 +41,7 @@ const routes: Routes = [
     InicioComponent,
     FooterComponent,
     LoginComponent,
+    AutosComponent,
     CitasComponent,
     PerfilComponent,
     AutosComponent,
@@ -71,7 +64,7 @@ const routes: Routes = [
     ReactiveFormsModule,
   ],
   exports: [RouterModule],
-  providers: [AuthService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 
