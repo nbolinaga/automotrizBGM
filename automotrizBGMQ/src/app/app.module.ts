92d3044dc0286090cca5fa1afd2a48f0637d403c
JendersonQuinter <j.quintero@correo.unimet.edu.ve>
2021-03-17 11:38:09 -0400

    Merge de Micale en Jenderson

--- automotrizBGMQ/src/app/app.module.ts
+++ automotrizBGMQ/src/app/app.module.ts
@@ -12,7 +12,6 @@ import { AngularFireAuthModule } from '@angular/fire/auth';
 import { environment } from 'src/environments/environment';
 
 import { InicioComponent } from './components/inicio/inicio.component';
-
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { LoginComponent } from './components/login/login.component';
@@ -21,17 +20,25 @@ import { PerfilComponent } from './components/perfil/perfil.component';
 import { AutosComponent } from './components/autos/autos.component';
 import { AdminComponent } from './components/admin/admin.component';
 import { CalendarioComponent } from './components/calendario/calendario.component';
-
-import {AuthService} from './services/auth.service';
-import { ReactiveFormsModule } from '@angular/forms';
 import { ContactanosComponent } from './components/contactanos/contactanos.component';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
 import { QuienesSomosComponent } from './components/quienes-somos/quienes-somos.component';
 import { VisitanosComponent } from './components/visitanos/visitanos.component';
 
+import {AuthService} from './services/auth.service';
+import { ReactiveFormsModule } from '@angular/forms';
+
 const routes: Routes = [
-  {path: '', component: InicioComponent},
+  {path: '', component: LandingPageComponent},
+  {path: 'inicio', component: InicioComponent},
+  {path: 'quienesSomos', component: QuienesSomosComponent},
+  {path: 'visitanos', component: VisitanosComponent},
+  {path: 'contactanos', component: ContactanosComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'citas', component: CitasComponent},
+  {path: 'perfil', component: PerfilComponent},
   {path: 'autos', component: AutosComponent},
+  {path: 'admin', component: AdminComponent},
   {path: 'calendario', component: CalendarioComponent}
 ];
 
