40c1266328b6ca3315e30dbf55048fc90720dacd
Norangel Marin <norangel.marin@correo.unimet.edu.ve>
2021-04-02 00:48:45 -0400

    ok

--- automotrizBGMQ/src/app/components/citas/citas.component.ts
+++ automotrizBGMQ/src/app/components/citas/citas.component.ts
@@ -57,6 +57,59 @@ citas: Cita[];
     }
   ];
 
+  citasSolicitudes: Array<any> = [
+    {
+      nombre: 'Pedro Perez',
+      motivo: 'Cambio de Aceite',
+      fecha: 'DD/MM/AA'
+    },
+    {
+      nombre: 'Maria Parra',
+      motivo: 'Servicio completo',
+      fecha: 'DD/MM/AA'
+    },
+    {
+      nombre: 'Carlos Chavez',
+      motivo: 'Cambio de Neumaticos',
+      fecha: 'DD/MM/AA'
+    },
+    {
+      nombre: 'Mario Benitez',
+      motivo: 'Revision',
+      fecha: 'DD/MM/AA'
+    },
+    {
+      nombre: 'Luis Alcala',
+      motivo: 'Cambio de pastillas de freno',
+      fecha: 'DD/MM/AA'
+    },
+    {
+      nombre: 'Marcos Mora',
+      motivo: 'Servicio completo',
+      fecha: 'DD/MM/AA'
+    },
+    {
+      nombre: 'Miguel Perensejo',
+      motivo: 'Reparación',
+      fecha: 'DD/MM/AA'
+    },
+    {
+      nombre: 'Alonso Hernandez',
+      motivo: 'Revision',
+      fecha: 'DD/MM/AA'
+    },
+    {
+      nombre: 'Camilo Turizo',
+      motivo: 'Cambio de aceite',
+      fecha: 'DD/MM/AA'
+    },
+    {
+      nombre: 'Matilda Hilda',
+      motivo: 'Servicio completo',
+      fecha: 'DD/MM/AA'
+    }
+  ];
+
   citasEstatus: Array<any> = [
     {
       nombre: 'Pedro Perez',
