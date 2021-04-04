862e54f991e56d303391ac9d3d3edad1885a807e
JendersonQuinter <j.quintero@correo.unimet.edu.ve>
2021-04-04 18:20:22 -0400

    Funcion para modificar las citas, errores pendientes por corregir

--- automotrizBGMQ/src/app/models/cita.ts
+++ automotrizBGMQ/src/app/models/cita.ts
@@ -1,10 +1,11 @@
-export interface Cita{
-  id: string;
-  cliente: string;
+export interface Cita {
+  id?: string;
+  idUser?: string;
+  cliente?: string;
   fecha?: string;
-  estado: string;
-  confirmada: boolean;
-  motivo: string;
-  descripcion: string;
-  vehiculo: string;
+  estado?: string;
+  confirmada?: boolean;
+  motivo?: string;
+  descripcion?: string;
+  vehiculo?: string;
 }
