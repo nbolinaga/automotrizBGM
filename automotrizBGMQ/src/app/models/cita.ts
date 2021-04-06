d2867a7e9b61b2639a620e11e69871b1de131aee
Norangel Marin <norangel.marin@correo.unimet.edu.ve>
2021-04-06 16:57:09 -0400

    cargando main

--- automotrizBGMQ/src/app/models/cita.ts
+++ automotrizBGMQ/src/app/models/cita.ts
@@ -3,18 +3,10 @@ export interface Cita {
   idUser?: string;
   cliente?: string;
   fecha?: string;
-<<<<<<< HEAD
   hora?: string;
   estado: string;
   confirmada: boolean;
   motivo: string;
   descripcion: string;
   vehiculo: string;
-=======
-  estado?: string;
-  confirmada?: boolean;
-  motivo?: string;
-  descripcion?: string;
-  vehiculo?: string;
->>>>>>> main
 }
