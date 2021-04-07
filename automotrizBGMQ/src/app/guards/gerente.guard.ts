0c8c0b665441a452e4f305c51d5ee02df7fa1531
Norangel Marin <norangel.marin@correo.unimet.edu.ve>
2021-04-07 01:19:20 -0400

    añadiendo guards

---
+++ automotrizBGMQ/src/app/guards/gerente.guard.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Usuario } from 'src/app/models/usuario';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GerenteGuard implements CanActivate {
+  usuario: Usuario;
+
+  constructor(private Router:Router){}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot):
+    Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+
+    if(this.usuario.rol=='Gerente'){
+      return true;
+    }
+    return this.Router.parseUrl('/perfil');
+  }  
+}
